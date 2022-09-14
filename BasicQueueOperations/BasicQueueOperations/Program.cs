ea5d684252e3bdd82600415cc336ba2d5c12b0fe
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-14 23:02:04 +0300

    Add files via upload

---
+++ BasicQueueOperations/BasicQueueOperations/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var intArrToManupolite = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> intQue = new Queue<int>();
+
+            int n = input[0];
+
+            int s = input[1];
+
+            int x = input[2];
+
+            for (int i = 0; i < n; i++)
+            {
+                intQue.Enqueue(intArrToManupolite[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                intQue.Dequeue();
+            }
+
+            int[] derivative = intQue.ToArray();
+
+            if (derivative.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int minValue = int.MaxValue;
+
+                if (!derivative.Any())
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    foreach (int item in derivative)
+                    {
+                        if (item < minValue)
+                        {
+                            minValue = item;
+                        }
+                    }
+                    Console.WriteLine(minValue);
+                }
+            }
+        }
+    }
+}
