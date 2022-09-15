cdb238093fbb579c98320af83aaccbdd927379f5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-15 13:15:52 +0300

    Add files via upload

---
+++ FastFoodOrders/FastFoodOrders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFoodOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(input);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                int recentOrder = orders.Peek();
+
+                if (recentOrder <= n)
+                {
+                    n -= recentOrder;
+                    orders.Dequeue();
+                    if (!orders.Any())
+                    {
+                        Console.WriteLine("Orders complete");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Orders left: {0}", string.Join(" ", orders));
+                    break;
+                }
+            }
+        }
+    }
+}
