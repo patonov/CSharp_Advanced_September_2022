1d962c6ceba6ac57265cb549a388a9c9b845cdde
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-17 18:22:36 +0300

    Add files via upload

---
+++ FashionBoutique/FashionBoutique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> cloths = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int rackCounter = 1;
+            int rackFillingSum = 0;
+
+            while (cloths.Any())
+            {
+                int currentCloth = cloths.Peek();
+
+                if (rackFillingSum + currentCloth <= rackCapacity)
+                {
+                    rackFillingSum += currentCloth;
+                    cloths.Pop();
+                }
+                else
+                {
+                    rackCounter++;
+                    rackFillingSum = 0;
+                }
+            
+            }
+
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
