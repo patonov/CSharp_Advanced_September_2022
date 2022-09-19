8ccb310667542a67b400fdd2dcf306a7a7d45cc9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-19 11:38:44 +0300

    Add files via upload

---
+++ BalancedParentheses/BalancedParentheses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            if (!input.Any())
+            {
+                return;
+            }
+
+            Stack<char> stack = new Stack<char>();
+            
+            foreach (char ch in input)
+            {
+                if (stack.Any())
+                {
+                    var currentChar = stack.Peek();
+
+                    if (currentChar == '{' && ch == '}')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (currentChar == '(' && ch == ')')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (currentChar == '[' && ch == ']')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (currentChar == ' ' && ch == ' ')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+                stack.Push(ch);
+            }
+
+            Console.WriteLine(stack.Any()? "NO": "YES");
+            
+        }
+    }
+}
