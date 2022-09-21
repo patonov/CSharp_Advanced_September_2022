b4a69b1dbbde022f69a6ebd402305fda95cd1835
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-21 17:43:54 +0300

    Add files via upload

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> charStack = new Stack<char>(Console.ReadLine().ToCharArray());
+
+            foreach(var ch in charStack) Console.Write(ch);
+        }
+    }
+}
