ec326654e918d6e72158533002aa1d7f334bbb71
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-23 21:38:16 +0300

    Add files via upload

---
+++ HashSetTest/HashSetTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetTest
+{
+    public class Person
+    { 
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> people = new HashSet<Person>();
+                       
+            people.Add(new Person { Name = "Ivan", Age = 35 });
+            people.Add(new Person { Name = "Ivan", Age = 35 });
+            people.Add(new Person { Name = "Ivan", Age = 35 });
+
+            Console.WriteLine(people.Count);
+
+            //Yes, there are three Persons with Name "Ivan" and Age 35 in the HashSet!
+        }
+    }
+}
