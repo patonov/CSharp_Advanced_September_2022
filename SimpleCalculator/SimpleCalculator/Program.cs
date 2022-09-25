c143d21b289d059a7d70f88523e732acbe97a5c2
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-25 15:00:07 +0300

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Stack<string> startArray = new Stack<string>(Console.ReadLine().Split(" ").Reverse().ToArray());
+
+            while (startArray.Count > 1)
+            {
+                int first = int.Parse(startArray.Pop());
+                string sign = startArray.Pop();
+                int second = int.Parse(startArray.Pop());
+                
+                if (sign == "+")
+                {
+                    first += second;
+                    startArray.Push(first.ToString());
+                }
+                else if (sign == "-")
+                {
+                    first -= second;
+                    startArray.Push(first.ToString());
+                }
+
+            }
+            Console.WriteLine(startArray.Peek());
+        }
+    }
+}
