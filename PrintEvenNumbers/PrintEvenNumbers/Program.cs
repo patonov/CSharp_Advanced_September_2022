bc12af5b9d7cdd73172f6b77207f800ea65129cb
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-25 16:32:42 +0300

    Add files via upload

---
+++ PrintEvenNumbers/PrintEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", new Queue<int>(Console.ReadLine().Split()
+                                                                                 .Select(int.Parse)
+                                                                                 .Where(n => n % 2 == 0).ToArray())));
+        }
+    }
+}
