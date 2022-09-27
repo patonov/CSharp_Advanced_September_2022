1fc75b523d066de0eee6c1b6b6840914075068aa
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-27 13:06:36 +0300

    Add files via upload

---
+++ DiagonalDifference/DiagonalDifference/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            int firstDiagonalSum = 0;
+            int secondDiagonalSum = 0;
+            int counter = n - 1;
+
+            for (int i = 0; i < n; i++)
+            {         
+                    firstDiagonalSum += matrix[i, i];
+                    secondDiagonalSum += matrix[i, counter];
+                    counter--;
+                
+            }
+
+            
+            Console.WriteLine(Math.Abs(firstDiagonalSum));
+            Console.WriteLine(Math.Abs(secondDiagonalSum));
+            Console.WriteLine(Math.Abs(firstDiagonalSum - secondDiagonalSum));
+        }
+    }
+}
