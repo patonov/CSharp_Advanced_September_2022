6103cd3949dedd9ef33ea9438f7d09c3090b18ef
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-09-27 13:24:27 +0300

    Add files via upload

---
+++ SquaresInMatrix/SquaresInMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            string[,] matrix = new string[rows, cols];
+            int counter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                var strings = Console.ReadLine().Split().ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = strings[col];
+                }
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
