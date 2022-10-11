d2cc6bb2929b1006b1112fc45e038945e2626156
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-10-11 21:20:10 +0300

    Add files via upload

---
+++ StreetRacing/StreetRacing/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private string licensePlate;
+        private int horsePower;
+        private double weight;
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Make: {this.Make}" + Environment.NewLine +
+                    $"Model: {this.Model}" + Environment.NewLine +
+                    $"License Plate: {this.LicensePlate}" + Environment.NewLine +
+                    $"Horse Power: {this.HorsePower}" + Environment.NewLine +
+                    $"Weight: {this.Weight}";
+
+        }
+    }
+}
---
+++ StreetRacing/StreetRacing/Race.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        private List<Car> participants;
+        private string name;
+        private string type;
+        private int laps;
+        private int capacity;
+        private int maxHorsePower;
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.participants = new List<Car>();
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+
+        public IEnumerable<Car> Participants => this.participants;
+
+        public int Count => this.participants.Count;
+
+        public bool Add(Car car)
+        {
+            string target = car.LicensePlate;
+            if (this.participants.Find(p => p.LicensePlate == target) == null && this.participants.Count < this.Capacity && car.HorsePower <= this.MaxHorsePower)
+            {
+                this.participants.Add(car);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            Car car = this.participants.Find(p => p.LicensePlate == licensePlate);
+            if (car != null)
+            {
+                this.participants.Remove(car);
+                return true;
+            }
+            return false;
+        }
+
+        public Car FindParticipant(string licensePlate)
+        { 
+        return this.participants.Find(p => p.LicensePlate == licensePlate);
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return this.participants.OrderByDescending(c => c.HorsePower).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+
+            foreach (Car c in this.participants)
+            {
+                sb.AppendLine(c.ToString());
+            }
+
+            string result = sb.ToString();
+            return result;
+        }
+    }
+}
