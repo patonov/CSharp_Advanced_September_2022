2dda8f454f8853b7491ed658a8005074789951b5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-10-16 17:21:03 +0300

    Add files via upload

---
+++ Openning/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        private string name;
+        private int age;
+        private string country;
+
+        public Employee(string name, int age, string country ) 
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {this.Name}, {this.Age} ({this.Country})";
+        }
+
+    }
+}
