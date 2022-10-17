4a93dafa599edccedf388a81c394682f0db28b34
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-10-17 17:42:18 +0300

    Add files via upload

---
+++ Classroom/Classroom.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (this.students.Count < this.Capacity)
+            {
+                this.students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            List<Student> targets = this.students.Where(x => x.FirstName == firstName).ToList();
+            Student target = targets.Find(x => x.LastName == lastName);
+
+            if (target != null)
+            {
+                this.students.Remove(target);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var targets = this.students.Where(x => x.Subject == subject).ToList();
+            if (targets.Any())
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+
+                foreach (Student st in targets)
+                {
+                    sb.AppendLine($"{st.FirstName} {st.LastName}");
+                }
+
+                return sb.ToString().Trim();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return this.students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student st = this.students.Find(x => x.FirstName == firstName && x.LastName == lastName);
+            return st;
+        }
+
+    }
+}
