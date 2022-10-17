c42935742a7629a1a6d9e8d10531395a7e39ffc4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-10-17 20:53:47 +0300

    Add files via upload

---
+++ Tuple/Tuple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Tuple<string> stringTuple = new Tuple<string>(input[0] + " " + input[1], input[2]);
+
+            string[] beer = Console.ReadLine().Split().ToArray();
+           
+            Tuple<string> beerTuple = new Tuple<string>(beer[0], beer[1]);
+
+            double[] doubleInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Tuple<double> doubleTuple = new Tuple<double>(doubleInput[0], doubleInput[1]);
+
+            Console.WriteLine(stringTuple);
+            Console.WriteLine(beerTuple);
+            Console.WriteLine(doubleTuple);
+
+        }
+    }
+}
---
+++ Tuple/Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<T>
+    {
+        public Tuple(T item1, T item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T item1 {get; set;}
+
+        public T item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2}";
+        }
+    }
+}
