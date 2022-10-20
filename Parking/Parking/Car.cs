ceba092aa92ff1a7214a9a6b445d0e56835663b9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-10-20 21:09:44 +0300

    Add files via upload

---
+++ Parking/Parking/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    public class Car
+    {
+        private string manufacturer;
+        private string model;
+        private int year;
+
+        public Car(string manufacturer, string model, int year)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Manufacturer} {this.Model} ({this.Year})";
+        }
+
+    }
+}
---
+++ Parking/Parking/Parking.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> data;
+        private string type; 
+        private int capacity;
+
+        public Parking(string type, int capacity)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.data = new List<Car>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+
+        public bool Add(Car car)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(car);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            var cars = this.data.Where(x => x.Manufacturer == manufacturer).ToList();
+            Car target = cars.Find(x => x.Model == model);
+
+            if (target != null)
+            {
+                this.data.Remove(target);
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public Car GetLatestCar()
+        {
+            if (this.data.Count > 0)
+            {
+                return this.data.OrderByDescending(x => x.Year).FirstOrDefault();
+            }
+            return null;
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            var cars = this.data.Where(x => x.Manufacturer == manufacturer).ToList();
+            Car target = cars.Find(x => x.Model == model);
+
+            if (target != null)
+            {                
+                return target;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int Count => this.data.Count;
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {this.Type}:");
+
+            foreach (Car car in this.data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
